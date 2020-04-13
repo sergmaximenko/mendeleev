40223030897f0747c4af6846c5e3596fb0c97b7f
sukavigimalka <jnew1@mail.ru>
2020-04-13 11:12:37 +0300

    add block 2

--- js/index.js
+++ js/index.js
@@ -24,31 +24,52 @@ function slider(){
 
 let timerId = setInterval(slider, 8000);
 
-
-let menu = document.querySelector('.menu');
+let menu = document.querySelector('.menu_wrap');
+(function(){
 setTimeout(function(){menu.scrollIntoView({behavior: "smooth"});}, 2500);
+setTimeout(function(){menu.classList.remove('display0');}, 2400);
+let loader = document.querySelector('.loader');
+setTimeout(function(){loader.classList.add('display0');}, 3000);
+})();
 
-
-let cont = document.querySelector('.cont');
 let mendeleev = document.querySelector('.mendeleev');
+let one = document.querySelector('.one');
+let coctails = document.querySelector('.coctails');
+let two = document.querySelector('.two');
 
 mendeleev.onclick = function(){
-	cont.classList.remove('display0');
-	cont.scrollIntoView({behavior: "smooth"});
+	one.classList.remove('display0');
+	one.scrollIntoView({behavior: "smooth"});
+	setTimeout(function(){menu.classList.add('display0');}, 700);
+	
+}
+coctails.onclick = function(){
+	two.classList.remove('display0');
+	two.scrollIntoView({behavior: "smooth"});
+	setTimeout(function(){menu.classList.add('display0');}, 700);
 }
 
+let arrow = document.querySelector('.arrow');
 let logo = document.querySelector('.logo_b');
-logo.onclick = function(){
-	menu.scrollIntoView({behavior: "smooth"});
-}
-
-let arrow = document.getElementById('arrow');
+let two_arrow = document.querySelector('.two_arrow');
+let two_logo = document.querySelector('.two_b_logo');
 arrow.onclick = function(){
+	menu.classList.remove('display0');
 	menu.scrollIntoView({behavior: "smooth"});
+	one.classList.add('display0');
 }
-
-let loader = document.querySelector('.loader');
-setTimeout(function(){loader.classList.add('display0');}, 3100);
-
-
-
+logo.onclick = function(){
+	menu.classList.remove('display0');
+	menu.scrollIntoView({behavior: "smooth"});
+	one.classList.add('display0');
+}
+two_arrow.onclick = function(){
+	menu.classList.remove('display0');
+	menu.scrollIntoView({behavior: "smooth"});
+	two.classList.add('display0');
+}
+two_logo.onclick = function(){
+	menu.classList.remove('display0');
+	menu.scrollIntoView({behavior: "smooth"});
+	two.classList.add('display0');
+}
